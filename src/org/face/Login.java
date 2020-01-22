2fe43ec4732167ca7064eb5446a14417274076d1
arrowraja <rajasekar.ccts@gmail.com>
2020-01-22 17:04:32 +0530

    updated

---
+++ src/org/face/Login.java
@@ -0,0 +1,17 @@
+package org.face;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sathi\\Desktop\\raja\\Facebook\\div\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+
+        driver.get("https://www.facebook.com/");
+        driver.findElement(By.id("email")).sendKeys("123@gmail.com"); 
+	}
+
+}
